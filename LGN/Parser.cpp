53e7649087f202574b43787bdc7ca4dbaf97beb8
NoKlyf <143846284+NoKlyf@users.noreply.github.com>
2023-09-23 16:44:15 +0200

    Added multiplication, division, subtraction, scopes and if statements

--- LGN/Parser.cpp
+++ LGN/Parser.cpp
@@ -1,4 +1,5 @@
 #include "Parser.h"
+#include "Lexer.h"
 using namespace lgn;
 
 std::optional<node::Program> Parser::parse()
@@ -32,43 +33,107 @@ std::optional<node::Term*> lgn::Parser::parse_term()
 		term->term = term_id;
 
 		return term;
+	} else if (auto tok_paren = try_consume(TokenType::tok_lparen)) {
+		auto expr = parse_expr();
+
+		if (!expr.has_value()) {
+			std::cerr << "Expected expression" << std::endl;
+			exit(EXIT_SUCCESS);
+		}
+
+		try_consume(TokenType::tok_rparen, "Expected ')'");
+
+		auto term_paren = m_allocator.alloc<node::TermParen>();
+		term_paren->expr = expr.value();
+
+		auto term = m_allocator.alloc<node::Term>();
+		term->term = term_paren;
+
+		return term;
 	}
 
 	return {};
 }
 
-std::optional<node::Expr*> Parser::parse_expr()
+std::optional<node::Expr*> Parser::parse_expr(int min_prec)
 {
-	if (auto term = parse_term()) {
-		if (try_consume(TokenType::tok_plus).has_value()) {
-			auto bin_expr = m_allocator.alloc<node::BinExpr>();
-			auto bin_expr_add = m_allocator.alloc<node::BinExprAdd>();
-			auto left = m_allocator.alloc<node::Expr>();
+	std::optional<node::Term*> term_left = parse_term();
 
-			left->expr = term.value();
-			bin_expr_add->left = left;
-
-			if (auto right = parse_expr()) {
-				bin_expr_add->right = right.value();
-				bin_expr->expr = bin_expr_add;
-
-				auto expr = m_allocator.alloc<node::Expr>();
-				expr->expr = bin_expr;
-
-				return expr;
-			}
-			
-			std::cerr << "Expected expression after '+'" << std::endl;
-			exit(EXIT_SUCCESS);
-		} else {
-			auto expr = m_allocator.alloc<node::Expr>();
-			expr->expr = term.value();
-
-			return expr;
-		}
+	if (!term_left.has_value()) {
+		return {};
 	}
 
-	return {};
+	auto expr_left = m_allocator.alloc<node::Expr>();
+	expr_left->expr = term_left.value();
+
+	while (true) {
+		std::optional<Token> curr_tok = peek();
+		std::optional<int> prec;
+
+		if (!curr_tok.has_value()) {
+			break;
+		}
+
+		prec = bin_prec(curr_tok->type);
+
+		if (!prec.has_value() || prec < min_prec) {
+			break;
+		}
+
+		Token op = consume();
+		int next_min_prec = prec.value() + 1;
+		auto expr_right = parse_expr(next_min_prec);
+
+		if (!expr_right.has_value()) {
+			std::cerr << "Unable to parse expression" << std::endl;
+			exit(EXIT_SUCCESS);
+		}
+
+		auto expr = m_allocator.alloc<node::BinExpr>();
+		auto expr_lhs = m_allocator.alloc<node::Expr>();
+
+		if (op.type == TokenType::tok_plus) {
+			auto add = m_allocator.alloc<node::BinExprAdd>();
+
+			expr_lhs->expr = expr_left->expr;
+
+			add->left = expr_lhs;
+			add->right = expr_right.value();
+			
+			expr->expr = add;
+		} else if (op.type == TokenType::tok_min) {
+			auto sub = m_allocator.alloc<node::BinExprSub>();
+
+			expr_lhs->expr = expr_left->expr;
+
+			sub->left = expr_lhs;
+			sub->right = expr_right.value();
+
+			expr->expr = sub;
+		} else if (op.type == TokenType::tok_mul) {
+			auto mul = m_allocator.alloc<node::BinExprMul>();
+
+			expr_lhs->expr = expr_left->expr;
+
+			mul->left = expr_lhs;
+			mul->right = expr_right.value();
+
+			expr->expr = mul;
+		} else if (op.type == TokenType::tok_div) {
+			auto div = m_allocator.alloc<node::BinExprDiv>();
+
+			expr_lhs->expr = expr_left->expr;
+
+			div->left = expr_lhs;
+			div->right = expr_right.value();
+
+			expr->expr = div;
+		}
+
+		expr_left->expr = expr;
+	}
+
+	return expr_left;
 }
 
 std::optional<node::BinExpr*> lgn::Parser::parse_bin_expr()
@@ -101,58 +166,112 @@ std::optional<node::BinExpr*> lgn::Parser::parse_bin_expr()
 
 std::optional<node::Statement*> lgn::Parser::parse_stmt()
 {
-	while (peek().has_value()) {
-		if (peek().value().type == TokenType::tok_kw) {
-			if (peek().value().value == "exit") {
-				consume();
+	if (peek().has_value() && peek().value().type == TokenType::tok_kw) {
+		if (peek().value().value == "exit") {
+			consume();
 
-				auto stmt_exit = m_allocator.alloc<node::StatementExit>();
+			auto stmt_exit = m_allocator.alloc<node::StatementExit>();
 
-				try_consume(TokenType::tok_lparen, "Expected '('");
+			try_consume(TokenType::tok_lparen, "Expected '('");
 
-				if (auto node_expr = parse_expr()) {
-					stmt_exit->expr = node_expr.value();
-				} else {
-					std::cerr << "Expected expression" << std::endl;
-					exit(EXIT_SUCCESS);
-				}
-
-				try_consume(TokenType::tok_rparen, "Expected ')'");
-				try_consume(TokenType::tok_semi, "Expected ';' at end-of-line");
-
-				auto stmt = m_allocator.alloc<node::Statement>();
-				stmt->statement = stmt_exit;
-
-				return stmt;
-			} else if (peek().value().value == "let") {
-				consume();
-
-				auto stmt_let = m_allocator.alloc<node::StatementLet>();
-				stmt_let->tok_id = try_consume(TokenType::tok_id, "Expected identifier");
-
-				try_consume(TokenType::tok_eq, "Expected identifier after '='");
-
-				if (auto node_expr = parse_expr()) {
-					stmt_let->expr = node_expr.value();
-				} else {
-					std::cerr << "Expected expression" << std::endl;
-				}
-
-				try_consume(TokenType::tok_semi, "Expected ';' at end-of-line");
-
-				auto stmt = m_allocator.alloc<node::Statement>();
-				stmt->statement = stmt_let;
-
-				return stmt;
+			if (auto node_expr = parse_expr()) {
+				stmt_exit->expr = node_expr.value();
+			} else {
+				std::cerr << "Expected expression" << std::endl;
+				exit(EXIT_SUCCESS);
 			}
+
+			try_consume(TokenType::tok_rparen, "Expected ')'");
+			try_consume(TokenType::tok_semi, "Expected ';' at end-of-line");
+
+			auto stmt = m_allocator.alloc<node::Statement>();
+			stmt->statement = stmt_exit;
+
+			return stmt;
+		} else if (peek().value().value == "let") {
+			consume();
+
+			auto stmt_let = m_allocator.alloc<node::StatementLet>();
+			stmt_let->tok_id = try_consume(TokenType::tok_id, "Expected identifier");
+
+			try_consume(TokenType::tok_eq, "Expected identifier after '='");
+
+			if (auto node_expr = parse_expr()) {
+				stmt_let->expr = node_expr.value();
+			} else {
+				std::cerr << "Expected expression" << std::endl;
+				exit(EXIT_SUCCESS);
+			}
+
+			try_consume(TokenType::tok_semi, "Expected ';' at end-of-line");
+
+			auto stmt = m_allocator.alloc<node::Statement>();
+			stmt->statement = stmt_let;
+
+			return stmt;
+		} else if (peek().value().value == "if") {
+			consume();
+			bool open_paren = false;
+			auto stmt_if = m_allocator.alloc<node::StatementIf>();
+
+			if (peek().value().type == TokenType::tok_lparen) {
+				consume();
+				open_paren = true;
+			}
+
+			if (auto expr = parse_expr()) {
+				stmt_if->expr = expr.value();
+			} else {
+				std::cerr << "Expected expression" << std::endl;
+				exit(EXIT_SUCCESS);
+			}
+
+			if (open_paren)
+				try_consume(TokenType::tok_rparen, "Expected ')'");
+
+			if (auto scope = parse_scope()) {
+				stmt_if->scope = scope.value();
+			} else {
+				std::cerr << "Expected '{'" << std::endl;
+				exit(EXIT_SUCCESS);
+			}
+
+			auto stmt = m_allocator.alloc<node::Statement>();
+			stmt->statement = stmt_if;
+
+			return stmt;
 		} else {
-			std::cerr << "Unkown keyword '" << peek().value().value.value() << "'" << std::endl;
+			std::cerr << "Unknown keyword '" << peek().value().value.value() << "'" << std::endl;
 			exit(EXIT_SUCCESS);
 		}
+	} else if (peek().has_value() && peek().value().type == TokenType::tok_lbrace) {
+		if (auto scope = parse_scope()) {
+			auto stmt = m_allocator.alloc<node::Statement>();
+			stmt->statement = scope.value();
+
+			return stmt;
+		}
+
+		std::cerr << "Expected '{'" << std::endl;
 	}
 
-	m_idx = 0;
 	return {};
+}
+
+std::optional<node::Scope*> lgn::Parser::parse_scope()
+{
+	if (!try_consume(TokenType::tok_lbrace))
+		return {};
+
+	auto scope = m_allocator.alloc<node::Scope>();
+
+	while (auto stmt = parse_stmt()) {
+		scope->statements.push_back(stmt.value());
+	}
+
+	try_consume(TokenType::tok_rbrace, "Expected '}'");
+
+	return scope;
 }
 
 std::optional<Token> Parser::peek(int count)
