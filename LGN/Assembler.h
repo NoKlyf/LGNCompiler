4a9bc2a93b09832a026a597ec4b5831f4f5070f2
NoKlyf <143846284+NoKlyf@users.noreply.github.com>
2023-09-23 16:46:20 +0200

    Added multiplication, division, subtraction, scopes and if statements

--- LGN/Assembler.h
+++ LGN/Assembler.h
@@ -2,7 +2,6 @@
 #include "Node.h"
 #include <sstream>
 #include <variant>
-#include <unordered_map>
 #include <iostream>
 
 namespace lgn
@@ -14,21 +13,34 @@ namespace lgn
 
 		std::string assemble();
 		void assemble_statement(const node::Statement *stmt);
-		void assemble_term(const node::Term* term);
+		void assemble_term(const node::Term *term);
 		void assemble_expr(const node::Expr *expr);
+		void assemble_bin_expr(const node::BinExpr* expr);
 
 	private:
 		struct Var {
+			std::string name;
 			size_t sp;
 		};
 
 		const node::Program m_prog;
 		std::stringstream m_output;
+
 		size_t m_ssize = 0;
-		std::unordered_map<std::string, Var> m_vars {};
+		int m_label_count = 0;
+
+		std::vector<Var> m_vars {};
+		std::vector<size_t> m_scopes {};
+
 		bool need_exit = true;
 
 		void push(const std::string& reg);
 		void pop(const std::string& reg);
+
+		void create_scope(const node::Scope* scope);
+		void begin_scope();
+		void end_scope();
+
+		std::string create_label();
 	};
 }
